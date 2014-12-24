bafb017cb52e41c6875e1f52b03c8c91b20e6184
rexey3s <rexey3s@gmail.com>
2014-12-24 22:00:49 +0700

    Trying to autowire editorKit

--- src/main/java/vn/edu/uit/owleditor/view/MainView.java
+++ src/main/java/vn/edu/uit/owleditor/view/MainView.java
@@ -6,6 +6,8 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.themes.ValoTheme;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.spring.UIScope;
 import org.vaadin.spring.navigator.VaadinView;
@@ -20,13 +22,14 @@ import vn.edu.uit.owleditor.core.OWLEditorKit;
 @VaadinView(name = MainView.NAME)
 public class MainView extends HorizontalLayout implements View {
     public final static String NAME = "mainView";
+    private static final Logger LOG = LoggerFactory.getLogger(MainView.class);
     final TabSheet root = new TabSheet();
 
     @Autowired
     OWLEditorKit editorKit;
     
     public MainView() {
-
+        LOG.info(editorKit.getActiveOntology().toString());
         root.addTab(new ClassesSheet(), "Classes");
         root.addTab(new ObjectPropertiesSheet(), "Object Properties");
         root.addTab(new DataPropertiesSheet(), "Data Properties");

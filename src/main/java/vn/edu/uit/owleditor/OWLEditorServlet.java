74a498e65b84dba136f557f626543a41ad692b96
rexey3s <rexey3s@gmail.com>
2014-12-27 16:11:03 +0700

    REST API now come true

---
+++ src/main/java/vn/edu/uit/owleditor/OWLEditorServlet.java
@@ -0,0 +1,12 @@
+package vn.edu.uit.owleditor;
+
+import org.vaadin.spring.servlet.SpringAwareVaadinServlet;
+
+/**
+ * @author Chuong Dang, University of Information and Technology, HCMC Vietnam,
+ *         Faculty of Computer Network and Telecomunication created on 12/27/14.
+ */
+
+public class OWLEditorServlet extends SpringAwareVaadinServlet {
+
+}

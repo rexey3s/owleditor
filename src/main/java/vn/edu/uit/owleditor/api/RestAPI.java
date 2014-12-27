59e0f5c3bdd84bcf3f716a3ed97584c9f4fad7b3
rexey3s <rexey3s@gmail.com>
2014-12-27 11:53:08 +0700

    Try to access static EditorKit getter in OWLEditorUI from a controller

--- src/main/java/vn/edu/uit/owleditor/api/RestAPI.java
+++ src/main/java/vn/edu/uit/owleditor/api/RestAPI.java
@@ -10,13 +10,12 @@ import org.semanticweb.owlapi.util.OWLClassExpressionVisitorAdapter;
 import org.semanticweb.owlapi.util.OWLObjectVisitorAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.vaadin.spring.UIScope;
-import vn.edu.uit.owleditor.OWLEditorUI;
 import vn.edu.uit.owleditor.core.OWLEditorKit;
 
 import javax.annotation.Nonnull;
@@ -31,7 +30,6 @@ import java.util.Set;
  */
 /* just an experimental controller*/
 @RestController
-@UIScope
 public class RestAPI {
     private static final int SIZE = 400;
     private static final Logger LOG = LoggerFactory.getLogger(RestAPI.class);
@@ -39,7 +37,9 @@ public class RestAPI {
     private final JsonArray thingArray = new JsonArray();
     private final Set<OWLClass> visited = new HashSet<>();
 
-
+    @Autowired
+    OWLEditorKit editorKit;
+    
     public static int randInt(int min, int max) {
 
         // NOTE: Usually this should be a field rather than a method
@@ -61,7 +61,7 @@ public class RestAPI {
 //        OWLEditorKit editorKit = (OWLEditorKit) session.getAttribute("kit");
 //        LOG.info(editorKit.getActiveOntology().toString());
         try {
-            Assert.notNull(OWLEditorUI.getEditorKit(), "Editor Kit should not be null");
+            Assert.notNull(editorKit, "Editor Kit should not be null");
             thingObject.addProperty("name", "Thing");
             thingObject.add("children", thingArray);
 //            ontology.accept(initPopulationEngine(ontology));

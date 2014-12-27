8da2d51ea1b966f2d90eb56928d5f9ffc1de1506
rexey3s <rexey3s@gmail.com>
2014-12-27 11:56:49 +0700

    Try to autowire editorKit to RestAPI

--- src/main/java/vn/edu/uit/owleditor/api/RestAPI.java
+++ src/main/java/vn/edu/uit/owleditor/api/RestAPI.java
@@ -64,7 +64,7 @@ public class RestAPI {
             Assert.notNull(editorKit, "Editor Kit should not be null");
             thingObject.addProperty("name", "Thing");
             thingObject.add("children", thingArray);
-//            ontology.accept(initPopulationEngine(ontology));
+            editorKit.getActiveOntology().accept(initPopulationEngine(editorKit.getActiveOntology()));
             return thingObject.toString();
         } catch (NullPointerException ex) {
             LOG.error(ex.getMessage());

230fe4a3a404b739d7fd6e0226582115d27b6cfd
rexey3s <rexey3s@gmail.com>
2015-01-29 16:26:55 +0700

    sth old but good

--- src/main/java/vn/edu/uit/owleditor/view/diagram/DnDTree.java
+++ src/main/java/vn/edu/uit/owleditor/view/diagram/DnDTree.java
@@ -8,7 +8,7 @@ import com.vaadin.ui.AbstractJavaScriptComponent;
  * @author Chuong Dang, University of Information and Technology, HCMC Vietnam,
  *         Faculty of Computer Network and Telecommunication created on 12/21/14.
  */
-@JavaScript({"lib/jquery.min.js", "lib/d3.v3.min.js", "lib/classTree.js"})
+@JavaScript({"lib/jquery.min.js", "lib/d3.v3.min.js", "lib/dndTree.js"})
 public class DnDTree extends AbstractJavaScriptComponent {
 
     public String getData() {
